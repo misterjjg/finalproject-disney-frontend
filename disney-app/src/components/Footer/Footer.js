ef759eb6c7abfd03fd0e3fcf7350602920ba4531
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-02-25 15:18:44 -0800

    updated footer/fonts

---
+++ disney-app/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>Developed by Joshua Gonzalez</div>
+      <div>2024</div>
+    </footer>
+  );
+};
