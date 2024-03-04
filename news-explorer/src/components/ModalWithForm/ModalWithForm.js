be55ea4c85c2558fd61df8fa74dcb5bd5af0bc79
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-03-03 17:02:23 -0800

    updated modals

--- news-explorer/src/components/ModalWithForm/ModalWithForm.js
+++ news-explorer/src/components/ModalWithForm/ModalWithForm.js
@@ -1,26 +1,58 @@
 import "./ModalWithForm.css";
+import CloseButton from "../../images/CloseButton.svg";
 
 const ModalWithForm = ({
   children,
   buttonText,
   title,
   onClose,
-  name,
-  isOpen,
   onSubmit,
+  onRegisterModal,
+  onLoginModal,
 }) => {
   return (
-    <div className={`modal modal_type_${name}`}>
-      <div className={`modal__content modal_container_${name} `}>
-        <button type="button" onClick={onClose} className="modal__close-button">
-          X
-        </button>
+    <div className={`modal`}>
+      <div className="modal__content">
+        <img
+          className="modal__close-button-logo"
+          onClick={onClose}
+          src={CloseButton}
+          alt={"Close Icon"}
+        />
         <h3 className="modal__title">{title}</h3>
-        <form className="modal__form" onSubmit={onSubmit}>
+        <form className="modal__labels" onClick={onSubmit}>
           {children}
-          <button type="submit" className="modal__submit-button">
-            {buttonText}
-          </button>
+          {title === "Sign In" ? (
+            <div className="modal__buttons">
+              <button className="modal__button-submit" type="submit">
+                {buttonText}
+              </button>
+              <div className="modal__button-other-option-content">
+                <p className="modal__button-other-option-or">or</p>
+                <button
+                  className="modal__button-other-option"
+                  onClick={onRegisterModal}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="modal__buttons">
+              <button className="modal__button-submit" type="submit">
+                {buttonText}
+              </button>
+              <div className="modal__button-other-option-content modal__button-other-option-signup">
+                <p className="modal__button-other-option-or">or</p>
+                <button
+                  className="modal__button-other-option"
+                  onClick={onLoginModal}
+                >
+                  Sign In
+                </button>
+              </div>
+            </div>
+          )}
         </form>
       </div>
     </div>
---
+++ news-explorer/src/components/RegisterModal/RegisterModal.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+
+const RegisterModal = ({ onClose, onRegister, isOpen }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassWord] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassWord(e.target.value);
+  };
+
+  const handleRegisterSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <ModalWithForm
+      title="Sign Up"
+      buttonText="Next"
+      className="modal__title"
+      onClose={onClose}
+      isOpen={isOpen}
+      onSubmit={handleRegisterSubmit}
+      onRegister={onRegister}
+    >
+      <fieldset className="modal__form-field">
+        <label className="modal__label">
+          Email
+          <input
+            className="modal__input"
+            type="email"
+            name="email"
+            minLength="1"
+            maxLength="60"
+            placeholder="Enter email"
+            value={email}
+            required={true}
+            onChange={handleEmailChange}
+          ></input>
+        </label>
+        <label className="modal__label">
+          Password
+          <input
+            className="modal__input"
+            type="text"
+            name="password"
+            minLength="1"
+            maxLength="30"
+            placeholder="Enter password"
+            value={password}
+            required={true}
+            onChange={handlePasswordChange}
+          ></input>
+        </label>
+        <label className="modal__label">
+          Username
+          <input
+            className="modal__input"
+            type="text"
+            name="name"
+            minLength="1"
+            maxLength="30"
+            placeholder="Enter username"
+            value={name}
+            required={true}
+            onChange={handleNameChange}
+          ></input>
+        </label>
+      </fieldset>
+    </ModalWithForm>
+  );
+};
+
+export default RegisterModal;
